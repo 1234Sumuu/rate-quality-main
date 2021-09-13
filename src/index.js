752d824cba106abc5bbf482206d47a8e45374275
1234Sumuu <sumaiya.akter.cse@ulab.edu.bd>
2021-09-12 20:03:10 -0700

    login page

---
+++ src/index.js
@@ -0,0 +1,26 @@
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./components/App";
+// import reportWebVitals from "./reportWebVitals";
+// import "bootstrap/dist/css/bootstrap.min.css";
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+
+// reportWebVitals();
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap/dist/css/bootstrap.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
